e0ba6d79df9a199ef61bcdf99753dcbb91bc19fc
b3stb3havi0ur <you@example.com>
2026-04-22 14:04:50 +0300

    added rust ffi

---
+++ rust-ffi/rust-ffi.c
@@ -0,0 +1,8 @@
+//
+// Created by larry on 4/22/26.
+//
+#include "rust-ffi.h"
+
+int add_r_ffi(int a, int b){
+    return a + b;
+}
---
+++ rust-ffi/rust-ffi.h
@@ -0,0 +1,10 @@
+//
+// Created by larry on 4/22/26.
+//
+
+#ifndef RUST_FFI_H
+#define RUST_FFI_H
+
+int add_r_ffi(int a, int b);
+
+#endif //RUST_FFI_H
